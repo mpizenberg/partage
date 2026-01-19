5d6ec3836277d21f55f0e7322c1abf3167767b88
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-01-19 18:29:18 +0100

    Split component styling into multiple files
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -8,6 +8,15 @@ import './ui/styles/variables.css';
 import './ui/styles/layout.css';
 import './ui/styles/components.css';
 
+// Import component-specific styles
+import './ui/styles/components/screens.css';
+import './ui/styles/components/members.css';
+import './ui/styles/components/balance.css';
+import './ui/styles/components/entries.css';
+import './ui/styles/components/forms.css';
+import './ui/styles/components/activities.css';
+import './ui/styles/components/settle.css';
+
 const root = document.getElementById('root');
 
 if (!root) {
